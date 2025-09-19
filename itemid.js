a3ba4d1dc2c8598e391f034aa8d64e1b0efc1097
norxdev <norxonics@gmail.com>
2025-09-19 08:28:08 -0400

    Update itemid.js

--- itemid.js
+++ itemid.js
@@ -4,9 +4,9 @@ const armorSetsData = [
   {
     name: "Sunfire Fanatic Armour Set",
     items: [
-      { name: "Helm", id: "28933", imgName: "Sunfire_fanatic_helm" },
-      { name: "Cuirass", id: "28936", imgName: "Sunfire_fanatic_cuirass" },
-      { name: "Chausses", id: "28939", imgName: "Sunfire_fanatic_chausses" }
+      { name: "Sunfire Fanatic Helm", id: "28933", imgName: "Sunfire_fanatic_helm" },
+      { name: "Sunfire Fanatic Cuirass", id: "28936", imgName: "Sunfire_fanatic_cuirass" },
+      { name: "Sunfire Fanatic Chausses", id: "28939", imgName: "Sunfire_fanatic_chausses" }
     ],
     setId: "29424",
     setImgName: "Sunfire_fanatic_armour_set"
@@ -14,9 +14,9 @@ const armorSetsData = [
   {
     name: "Blood Moon Armour Set",
     items: [
-      { name: "Chestplate", id: "29022", imgName: "Blood_moon_chestplate_detail" },
-      { name: "Helm", id: "29028", imgName: "Blood_moon_helm_detail" },
-      { name: "Tassets", id: "29025", imgName: "Blood_moon_tassets_detail" },
+      { name: "Blood Moon Chestplate", id: "29022", imgName: "Blood_moon_chestplate_detail" },
+      { name: "Blood Moon Helm", id: "29028", imgName: "Blood_moon_helm_detail" },
+      { name: "Blood Moon Tassets", id: "29025", imgName: "Blood_moon_tassets_detail" },
       { name: "Dual Macuahuitl", id: "28997", imgName: "Dual_macuahuitl_detail" }
     ],
     setId: "31136",
@@ -25,10 +25,10 @@ const armorSetsData = [
   {
     name: "Blue Moon Armour Set",
     items: [
-      { name: "Chestplate", id: "29013", imgName: "Blue_moon_chestplate_detail" },
-      { name: "Helm", id: "29019", imgName: "Blue_moon_helm_detail" },
-      { name: "Spear", id: "28988", imgName: "Blue_moon_spear_detail" },
-      { name: "Tassets", id: "29016", imgName: "Blue_moon_tassets_detail" }
+      { name: "Blue Moon Chestplate", id: "29013", imgName: "Blue_moon_chestplate_detail" },
+      { name: "Blue Moon Helm", id: "29019", imgName: "Blue_moon_helm_detail" },
+      { name: "Blue Moon Spear", id: "28988", imgName: "Blue_moon_spear_detail" },
+      { name: "Blue Moon Tassets", id: "29016", imgName: "Blue_moon_tassets_detail" }
     ],
     setId: "31139",
     setImgName: "Blue_moon_armour_set_detail"
@@ -36,10 +36,10 @@ const armorSetsData = [
   {
     name: "Eclipse Moon Armour Set",
     items: [
-      { name: "Chestplate", id: "29004", imgName: "Eclipse_moon_chestplate_detail" },
-      { name: "Helm", id: "29010", imgName: "Eclipse_moon_helm_detail" },
-      { name: "Tassets", id: "29007", imgName: "Eclipse_moon_tassets_detail" },
-      { name: "Atlatl", id: "29000", imgName: "Eclipse_atlatl_detail" }
+      { name: "Eclipse Moon Chestplate", id: "29004", imgName: "Eclipse_moon_chestplate_detail" },
+      { name: "Eclipse Moon Helm", id: "29010", imgName: "Eclipse_moon_helm_detail" },
+      { name: "Eclipse Moon Tassets", id: "29007", imgName: "Eclipse_moon_tassets_detail" },
+      { name: "Eclipse Atlatl", id: "29000", imgName: "Eclipse_atlatl_detail" }
     ],
     setId: "31142",
     setImgName: "Eclipse_moon_armour_set_detail"
