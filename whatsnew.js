24c09436e825fa53ed5c47f3fd1d7e6817b85dd4
norxdev <norxonics@gmail.com>
2026-01-09 14:53:03 -0500

    Add new update messages to whatsnew.js

--- whatsnew.js
+++ whatsnew.js
@@ -2,5 +2,6 @@
 window.whatsNewItems = [
   "Introducing the 'What's New' button. Quickly see all recent updates to the site!",
   "Easily switch between 'Buy Pieces → Sell Set' and 'Buy Set → Sell Pieces' modes. This was a top requested addition!",
-  "General styling improvements for mobile devices."
+  "General styling improvements for mobile devices.",
+  "Tax is now visible! This will make it easier to understand the breakout of trades."
 ];
