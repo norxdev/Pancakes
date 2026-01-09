39d0c1d7d735afd0ccbe55479f0273d8424cd4cd
norxdev <norxonics@gmail.com>
2026-01-09 10:11:08 -0500

    Add whatsnew.js for recent site updates

---
+++ whatsnew.js
@@ -0,0 +1,6 @@
+// whatsnew.js
+window.whatsNewItems = [
+  "Introducing the 'What's New' button. Quickly see all recent updates to the site!",
+  "Easily switch between 'Buy Pieces → Sell Set' and 'Buy Set → Sell Pieces' modes. This was a top requested addition!",
+  "General styling improvements for mobile devices."
+];
