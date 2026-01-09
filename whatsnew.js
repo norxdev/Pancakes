f5e5b3650c68bcc7252e5812354c66fdafad6bf1
norxdev <norxonics@gmail.com>
2026-01-09 15:09:57 -0500

    Add new feedback message to whatsNewItems

--- whatsnew.js
+++ whatsnew.js
@@ -1,5 +1,6 @@
 // whatsnew.js
 window.whatsNewItems = [
+  "I'm glad you are enjoying ArmourFlipper! Continue to share feedback, it helps me make this tool even better!",
   "Introducing the 'What's New' button. Quickly see all recent updates to the site!",
   "Easily switch between 'Buy Pieces → Sell Set' and 'Buy Set → Sell Pieces' modes. This was a top requested addition!",
   "General styling improvements for mobile devices.",
