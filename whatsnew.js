182b2a7924742bc5b5ce52675683628ec041f6c7
norxdev <norxonics@gmail.com>
2026-08-19 20:40:57 -0400

    Update whatsnew.js

--- whatsnew.js
+++ whatsnew.js
@@ -1,6 +1,6 @@
 // whatsnew.js
 window.whatsNewItems = [
-  "I'm glad you are enjoying ArmourFlipper! Continue to share feedback, it helps me make this tool even better!",
+  //"I'm glad you are enjoying ArmourFlipper! Continue to share feedback, it helps me make this tool even better!",
   "General site maintenance.",
   "Added additional item sets."
 ];
