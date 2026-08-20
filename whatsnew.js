23c1ff6080e0f06d3b15da389ba9f4c3a4d35604
norxdev <norxonics@gmail.com>
2026-08-19 20:36:02 -0400

    Update whatsnew.js

--- whatsnew.js
+++ whatsnew.js
@@ -1,8 +1,6 @@
 // whatsnew.js
 window.whatsNewItems = [
   "I'm glad you are enjoying ArmourFlipper! Continue to share feedback, it helps me make this tool even better!",
-  "Introducing the 'What's New' button. Quickly see all recent updates to the site!",
-  "Easily switch between 'Buy Pieces → Sell Set' and 'Buy Set → Sell Pieces' modes. This was a top requested addition!",
-  "General styling improvements for mobile devices.",
-  "Tax is now visible! This will make it easier to understand the breakout of flips."
+  "General site maintenance.",
+  "Added additional item sets."
 ];
